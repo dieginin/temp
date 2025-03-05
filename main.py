ec23ca90aa0ab6f3cc0a28009678bc314228a611
dieginin <diegobalestra9@gmail.com>
2025-03-05 01:30:52 -0800

    0.1.33

--- main.py
+++ main.py
@@ -3,47 +3,48 @@ import sys
 import time
 
 import flet as ft
-import requests
+
+# import requests
 from config import VERSION
 
 local_version = VERSION
 
-def get_latest_release():
-    # URL de la API para obtener la última versión del release de tu repositorio
-    url = "https://api.github.com/repos/dieginin/temp/releases/latest"
-    response = requests.get(url)
-    if response.status_code == 200:
-        release_data = response.json()
-        return release_data["tag_name"], release_data["assets"]
-    else:
-        return "No disponible", []
+# def get_latest_release():
+#     # URL de la API para obtener la última versión del release de tu repositorio
+#     url = "https://api.github.com/repos/dieginin/temp/releases/latest"
+#     response = requests.get(url)
+#     if response.status_code == 200:
+#         release_data = response.json()
+#         return release_data["tag_name"], release_data["assets"]
+#     else:
+#         return "No disponible", []
 
 
-def download_asset(asset_url, file_name):
-    # Realiza la solicitud para descargar el archivo
-    response = requests.get(asset_url, stream=True)
-    if response.status_code == 200:
-        # Guarda el archivo descargado (reemplazando el existente)
-        with open(file_name, "wb") as file:
-            for chunk in response.iter_content(chunk_size=1024):
-                if chunk:
-                    file.write(chunk)
-        return True
-    else:
-        return False
+# def download_asset(asset_url, file_name):
+#     # Realiza la solicitud para descargar el archivo
+#     response = requests.get(asset_url, stream=True)
+#     if response.status_code == 200:
+#         # Guarda el archivo descargado (reemplazando el existente)
+#         with open(file_name, "wb") as file:
+#             for chunk in response.iter_content(chunk_size=1024):
+#                 if chunk:
+#                     file.write(chunk)
+#         return True
+#     else:
+#         return False
 
 
-def restart_app():
-    """Reinicia la aplicación"""
-    time.sleep(1)  # Espera un poco antes de reiniciar
-    os.execv(sys.executable, ["python"] + sys.argv)  # Reinicia el script
+# def restart_app():
+#     """Reinicia la aplicación"""
+#     time.sleep(1)  # Espera un poco antes de reiniciar
+#     os.execv(sys.executable, ["python"] + sys.argv)  # Reinicia el script
 
 
-def compare_versions(current_version, latest_version):
-    # Compara dos versiones en formato 'X.Y.Z'
-    current_version_parts = [int(part) for part in current_version.split(".")]
-    latest_version_parts = [int(part) for part in latest_version.split(".")]
-    return latest_version_parts > current_version_parts
+# def compare_versions(current_version, latest_version):
+#     # Compara dos versiones en formato 'X.Y.Z'
+#     current_version_parts = [int(part) for part in current_version.split(".")]
+#     latest_version_parts = [int(part) for part in latest_version.split(".")]
+#     return latest_version_parts > current_version_parts
 
 
 def main(page: ft.Page):
@@ -52,16 +53,16 @@ def main(page: ft.Page):
 
     txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
 
-    # Obtener la última versión del release de GitHub
-    latest_version, assets = get_latest_release()
+    # # Obtener la última versión del release de GitHub
+    # latest_version, assets = get_latest_release()
 
-    # Comparar las versiones
-    if compare_versions(local_version, latest_version):
-        # Si la versión más reciente es mayor que la local, habilitar la descarga
-        update_available = True
-    else:
-        # Si la aplicación ya está actualizada
-        update_available = False
+    # # Comparar las versiones
+    # if compare_versions(local_version, latest_version):
+    #     # Si la versión más reciente es mayor que la local, habilitar la descarga
+    #     update_available = True
+    # else:
+    #     # Si la aplicación ya está actualizada
+    #     update_available = False
 
     def minus_click(e):
         txt_number.value = str(int(txt_number.value) - 1)
@@ -71,48 +72,48 @@ def main(page: ft.Page):
         txt_number.value = str(int(txt_number.value) + 1)
         page.update()
 
-    def download_click(e):
-        if assets:
-            # Tomamos el primer asset (puedes modificar esto si hay más de un asset)
-            asset_url = assets[0]["browser_download_url"]
-            file_name = assets[0]["name"]
+    # def download_click(e):
+    #     if assets:
+    #         # Tomamos el primer asset (puedes modificar esto si hay más de un asset)
+    #         asset_url = assets[0]["browser_download_url"]
+    #         file_name = assets[0]["name"]
 
-            # Descargar y reemplazar el archivo
-            success = download_asset(asset_url, file_name)
-            if success:
-                page.add(
-                    ft.Text(
-                        f"Archivo {file_name} descargado y reemplazado exitosamente.",
-                        size=18,
-                    )
-                )
+    #         # Descargar y reemplazar el archivo
+    #         success = download_asset(asset_url, file_name)
+    #         if success:
+    #             page.add(
+    #                 ft.Text(
+    #                     f"Archivo {file_name} descargado y reemplazado exitosamente.",
+    #                     size=18,
+    #                 )
+    #             )
 
-                # Reiniciar la aplicación
-                restart_app()
-            else:
-                page.add(ft.Text("Error al descargar el archivo.", size=18))
-        else:
-            page.add(ft.Text("No hay archivos disponibles para descargar.", size=18))
+    #             # Reiniciar la aplicación
+    #             restart_app()
+    #         else:
+    #             page.add(ft.Text("Error al descargar el archivo.", size=18))
+    #     else:
+    #         page.add(ft.Text("No hay archivos disponibles para descargar.", size=18))
 
-    # Mostrar mensaje si la app está actualizada o no
-    if update_available:
-        update_message = ft.Text(
-            f"Hay una nueva versión disponible: {latest_version}",
-            size=20,
-            weight=ft.FontWeight.BOLD,
-        )
-        update_button = ft.ElevatedButton(
-            "Descargar última versión", on_click=download_click
-        )
-    else:
-        update_message = ft.Text(
-            f"Tu aplicación está actualizada. Versión actual: {local_version}",
-            size=20,
-            weight=ft.FontWeight.BOLD,
-        )
-        update_button = ft.ElevatedButton(
-            "No hay actualizaciones disponibles", disabled=True
-        )
+    # # Mostrar mensaje si la app está actualizada o no
+    # if update_available:
+    #     update_message = ft.Text(
+    #         f"Hay una nueva versión disponible: {latest_version}",
+    #         size=20,
+    #         weight=ft.FontWeight.BOLD,
+    #     )
+    #     update_button = ft.ElevatedButton(
+    #         "Descargar última versión", on_click=download_click
+    #     )
+    # else:
+    #     update_message = ft.Text(
+    #         f"Tu aplicación está actualizada. Versión actual: {local_version}",
+    #         size=20,
+    #         weight=ft.FontWeight.BOLD,
+    #     )
+    #     update_button = ft.ElevatedButton(
+    #         "No hay actualizaciones disponibles", disabled=True
+    #     )
 
     page.add(
         ft.Row(
@@ -123,8 +124,8 @@ def main(page: ft.Page):
             ],
             alignment=ft.MainAxisAlignment.CENTER,
         ),
-        update_message,
-        update_button,
+        # update_message,
+        # update_button,
     )
 
 
