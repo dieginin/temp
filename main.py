7b38cf6a848fedf2b0d4022de254c070006341cc
dieginin <diegobalestra9@gmail.com>
2025-03-08 02:02:50 -0800

    update config.py and main.py

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ from controllers import Updater
 def main(page: ft.Page):
     """Interfaz principal con botón de verificación de actualizaciones"""
     page.title = "Flet App"
-    updater = Updater()
 
     def check_for_updates(e):
-        updater.check_for_updates(page)
+        updater.check_for_updates()
 
-    page.add(ft.Text(f"MyApp v{VERSION}!!!"))
+    page.add(ft.Text(f"MyApp v{VERSION}"))
     page.add(ft.ElevatedButton("Buscar actualizaciones", on_click=check_for_updates))
+
+    updater = Updater(page)
 
 
 ft.app(main)
