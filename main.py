f37a874d385d10f20019f8306b87ba1b34e368f5
dieginin <diegobalestra9@gmail.com>
2025-04-02 23:29:10 -0700

    update build-desktop.yml, config.py and main.py

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ from controllers import Updater
 def main(page: ft.Page) -> None:
     page.title = "Flet App"
 
-    page.add(ft.Text(f"MyApp v{VERSION} NEW"))
+    page.add(ft.Text(f"MyApp v{VERSION}"))
 
     Updater(page)
